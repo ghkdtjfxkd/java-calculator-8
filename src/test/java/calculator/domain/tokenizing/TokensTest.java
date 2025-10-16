79ee03397a1edfc01f2146622c8634eaf3e1a516
ghkdtjfxkd <syt1128@gmail.com>
2025-10-16 17:12:18 +0900

    fix: 토큰화 과정에서 발생하던 검증 문제 해결
    - `//\n12` 입력 시 `결과 : 12`로 출력됨.
    - 커스텀 구분자 형식(문자열 길이)에 맞지 않음으로 에러를 발생시켜야 함.
    - Operator, Operand, Calculation 문제인줄 알았으나 토큰화 과정에서 검증 로직 누락

--- src/test/java/calculator/domain/tokenizing/TokensTest.java
+++ src/test/java/calculator/domain/tokenizing/TokensTest.java
@@ -3,9 +3,23 @@ package calculator.domain.tokenizing;
 import static org.junit.jupiter.api.Assertions.*;
 
 import calculator.domain.delimiter.Delimiters;
+import calculator.domain.rawinput.Formula;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TokensTest {
 
+    @Test
+    @DisplayName("사용자의_입력이_null_이라면_예외를_발생시킨다.")
+    void userInputIsNull() {
+        String input = null;
+        assertThrows(IllegalArgumentException.class, () -> Formula.from(input));
+    }
+
+    @Test
+    @DisplayName("숫자나 구분자로 지정되지 않은 문자가 식에 포함되어 있다면 예외를 발생시켜야 한다.")
+    void correctDelimiterTokenizeTest() {
+        String input = "1,2,3|";
+        assertThrows(IllegalArgumentException.class, () -> Tokens.of(input, Delimiters.defaults()));
+    }
 }
